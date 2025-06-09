2ffe530f869d551e32764770820b7e09d8e6bebe
Oren Hadar <orenhd123@gmail.com>
2025-06-09 14:50:33 +0300

    Fix life indicators to match C# version with triangular edges

--- src/game/ui/Life.ts
+++ src/game/ui/Life.ts
@@ -1,13 +1,35 @@
 import { SpriteState } from '../../constants';
+import { Vector2D } from '../../types/Vector2D';
 
 export class Life {
-    private readonly size = { width: 48, height: 24 };
+    private readonly size = { width: 30, height: 10 };
     private readonly color = 'lightpink';
+    private readonly edgeColor = 'lightcyan';
     private readonly x: number;
+    private readonly y: number = 10;
     private state: SpriteState = SpriteState.Alive;
+    private leftEdge: Vector2D[] = [];
+    private rightEdge: Vector2D[] = [];
 
     constructor(x: number) {
         this.x = x;
+        this.buildEdges();
+    }
+
+    private buildEdges(): void {
+        // Left edge triangle points
+        this.leftEdge = [
+            new Vector2D(this.x, this.y),
+            new Vector2D(this.x, this.y + this.size.height),
+            new Vector2D(this.x - this.size.height, this.y + this.size.height)
+        ];
+
+        // Right edge triangle points
+        this.rightEdge = [
+            new Vector2D(this.x + this.size.width, this.y),
+            new Vector2D(this.x + this.size.width, this.y + this.size.height),
+            new Vector2D(this.x + this.size.width + this.size.height, this.y + this.size.height)
+        ];
     }
 
     public getState(): SpriteState {
@@ -22,8 +44,29 @@ export class Life {
         if (this.state === SpriteState.Dead) return;
 
         ctx.save();
+
+        // Draw left edge triangle
+        ctx.fillStyle = this.edgeColor;
+        ctx.beginPath();
+        ctx.moveTo(this.leftEdge[0].x, this.leftEdge[0].y);
+        ctx.lineTo(this.leftEdge[1].x, this.leftEdge[1].y);
+        ctx.lineTo(this.leftEdge[2].x, this.leftEdge[2].y);
+        ctx.closePath();
+        ctx.fill();
+
+        // Draw main body
         ctx.fillStyle = this.color;
-        ctx.fillRect(this.x, 10, this.size.width, this.size.height);
+        ctx.fillRect(this.x, this.y, this.size.width, this.size.height);
+
+        // Draw right edge triangle
+        ctx.fillStyle = this.edgeColor;
+        ctx.beginPath();
+        ctx.moveTo(this.rightEdge[0].x, this.rightEdge[0].y);
+        ctx.lineTo(this.rightEdge[1].x, this.rightEdge[1].y);
+        ctx.lineTo(this.rightEdge[2].x, this.rightEdge[2].y);
+        ctx.closePath();
+        ctx.fill();
+
         ctx.restore();
     }
 } 
